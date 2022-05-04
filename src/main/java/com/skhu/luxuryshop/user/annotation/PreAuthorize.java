446ece147a9396f745127298db79e97c3305ae66
TAEJIN <ctj7743@naver.com>
2022-05-04 19:39:36 +0900

    크롤링 왜 안올라가

--- src/main/java/com/skhu/luxuryshop/user/annotation/PreAuthorize.java
+++ src/main/java/com/skhu/luxuryshop/user/annotation/PreAuthorize.java
@@ -6,8 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /* @Target = 어노테이션이 사용될 위치를 지정한다. */
+/* ElementType.METHOD = 메소드에만 어노테이션을 붙일 수 있다. */
 @Target(ElementType.METHOD)
 /* @Retention = 어느 시점까지 어노테이션의 메모리를 가져갈지 설정한다. */
+/* RetentionPolicy.RUNTIME
+= JVM이 실제 클래스의 자바 바이트코드를 해석해서 동작하는 Runtime단계에서 메모리가 유지되고,
+Runtime이 종료되면 메모리도 사라진다. */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PreAuthorize {
     String[] roles();
--- src/main/java/com/skhu/luxuryshop/user/jwt/AuthInterceptor.java
+++ src/main/java/com/skhu/luxuryshop/user/jwt/AuthInterceptor.java
@@ -30,10 +30,13 @@ public class AuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) {
         HandlerMethod method = (HandlerMethod) handler;
+        /* Optional = nullpointexception을 방지하여 데이터를 가져오기 위해 사용하는 클래스 */
         Optional<PreAuthorize> preAuthorize = getPreAuthorize(method);
         Optional<String> jwt = resolveToken(request);
 
+        /* isPresent()로 Optional 객체가 null인지 아닌지 확인하고 있으면 get()으로 저장된 값을 가져온다. */
         if (preAuthorize.isPresent()) {
+            /* orElseThrow() = 예외처리 / jwt에 토큰이 안담겨 있으면 (토큰이 존재하지 않습니다.) 예외처리 */
             String token = jwt.orElseThrow(NoTokenException::new);
             tokenProvider.validateToken(token);
             return AnnotationHandler.hasAnyRole(getAuthorities(token), preAuthorize.get());
@@ -43,7 +46,13 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     private Optional<String> resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        /* StringUtils.hasText = null이 아니여야 하고, 길이가 0보다 커야하고, 공백이 아닌 문자가 포함되어있는지 검증한다. */
+        /* startsWith(TOKEN_HEADER) = 문자열이 "Bearer "로 시작하는지 검증한다. */
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(TOKEN_HEADER)) {
+            /* Optional.of = null이 아닌 객체를 담고 있는 Optional 객체를 생성한다. */
+            /* substring() = 문자열에서 특정한 구간의 문자열을 추출한다. */
+            /* length() = 문자열의 길이 */
+            /* "Bearer "을 제외하고 그 뒤의 문자열을 resolveToken으로 만든다. */
             return Optional.of(bearerToken.substring(TOKEN_HEADER.length()));
         }
         return Optional.empty();
@@ -54,12 +63,14 @@ public class AuthInterceptor implements HandlerInterceptor {
         return authorities.split(AUTHORITIES_SPLITTER);
     }
 
-    private Optional<PreAuthorize> getPreAuthorize(HandlerMethod method) {
+    private Optional<PreAuthorize>
+    getPreAuthorize(HandlerMethod method) {
         for (Annotation annotation : method.getMethod().getAnnotations()) {
             if (annotation instanceof PreAuthorize) {
                 return Optional.of((PreAuthorize) annotation);
             }
         }
+        /* 빈 객체를 리턴해준다 */
         return Optional.empty();
     }
 }
